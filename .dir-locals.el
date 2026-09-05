2a2c155628f1d4c9338bbd80451b726983205c56
Tristan <trist007@darkterminal.net>
2026-09-04 21:05:35 -0500

    ok got it working

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ((nil . ((eval . (progn
-  (let ((root (locate-dominating-file default-directory "build.bat")))
-    (when (stringp root)
-      (setq-local default-directory root)))
+  ;;(let ((root (locate-dominating-file default-directory "build.bat")))
+  ;;  (when (stringp root)
+  (setq-local my-terrain-project-root "c:/dev/cielo/cielo/code/")
   (define-minor-mode my-terrain-project-mode
     "Local keybindings for this project directory."
     :lighter ""
@@ -10,12 +10,11 @@
                 (lambda () (interactive) (compile "build.bat")))
               (define-key map (kbd "<f2>")
                 (lambda () (interactive)
-                  (start-process "terrain" nil
-                                 (expand-file-name "terrain.exe" default-directory))))
+                  (start-process "terrain" "+terrain-output*"
+                                 (expand-file-name "run.bat" default-directory))))
               (define-key map (kbd "<f3>")
                 (lambda () (interactive)
                   (start-process "raddbg" nil "raddbg.exe"
                                  (expand-file-name "terrain.exe" default-directory))))
               map))
-  ;(my-terrain-project-mode 1))))))
-  (run-with-idle-timer 0 nil #'my-terrain-project-mode 1))))))
+  (my-terrain-project-mode 1))))))
